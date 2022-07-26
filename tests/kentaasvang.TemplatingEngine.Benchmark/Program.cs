f6f4c2caa45962d8703f25a4ca166431f2fa19aa
Kent Martin Åsvang <kentaasvang@gmail.com>
2022-07-26 23:42:57 +0200

    basic benchmark

---
+++ tests/kentaasvang.TemplatingEngine.Benchmark/Program.cs
@@ -0,0 +1,20 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace kentaasvang.TemplatingEngine.Benchmark;
+
+public class Benchmarks
+{
+    [Benchmark]
+    public void Replace()
+    {
+        TemplatingEngine templateEngine = new();
+        var template = "Should be a long ass text [key] with a lot of values";
+        templateEngine.LoadTemplate(template);
+        Dictionary<string, string> keyVals = new() {{"key", "value"}};
+        var result = templateEngine.Replace(keyVals);
+    }
+
+
+    static void Main() => BenchmarkRunner.Run<Benchmarks>();
+}
